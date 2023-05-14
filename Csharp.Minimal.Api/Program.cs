8a583e3c74bad3dadf135521ea38e017496b08c2
meyerhenning <henning@sealedcore.tech>
2023-05-14 15:32:00 +0200

    feat(Csharp.Minimal): add swagger

--- Csharp.Minimal.Api/Program.cs
+++ Csharp.Minimal.Api/Program.cs
@@ -7,7 +7,16 @@ builder.Services.AddDbContext<ApiContext>(
     o =>
         o.UseInMemoryDatabase("ApiDb"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
