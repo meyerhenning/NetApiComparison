b9e0c05b41319a0e6a8add7c0a09fbd1125d1179
meyerhenning <henning@sealedcore.tech>
2023-05-14 18:33:45 +0200

    feat(Csharp.Minimal): add Students endpoints

--- Csharp.Minimal.Api/Program.cs
+++ Csharp.Minimal.Api/Program.cs
@@ -1,4 +1,5 @@
 using Csharp.Minimal.Api;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,70 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
+// Gets all students
+app.MapGet("/Students",
+    async (ApiContext context) 
+        => await context.Students.ToListAsync());
+
+// Gets a single student by id
+app.MapGet("/Students/{id:guid}",
+    async (ApiContext context, [FromRoute] Guid id) =>
+    {
+        var student =
+            await context.Students.FindAsync(id);
+
+        return student is null
+            ? Results.NotFound()
+            : Results.Ok(student);
+    });
+
+// Adds a student
+app.MapPost("/Students",
+    async (ApiContext context, [FromBody] Student newStudent) =>
+    {
+        context.Students.Add(newStudent);
+        await context.SaveChangesAsync();
+        
+        return Results.Created(
+            $"/Students/{newStudent.Id}",
+            newStudent);
+});
+
+// Updates a student
+app.MapPut("/Students/{id:guid}",
+    async (ApiContext context, [FromRoute] Guid id, [FromBody] Student updatedStudent) =>
+    {
+        var student =
+            await context.Students.FindAsync(id);
+
+        if (student is null)
+        {
+            return Results.NotFound();
+        }
+
+        student.FirstName = updatedStudent.FirstName;
+        student.LastName = updatedStudent.LastName;
+
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    });
+
+// Deletes a student
+app.MapDelete("/Students/{id:guid}",
+    async (ApiContext context, [FromRoute] Guid id) =>
+    {
+        var student =
+            await context.Students.FindAsync(id);
+
+        if (student is null)
+        {
+            return Results.NotFound();
+        }
+
+        context.Students.Remove(student);
+        await context.SaveChangesAsync();
+
+        return Results.Ok(student);
+    });
 
 app.Run();
