9b81d76ff00d0b6c3b971cc93049d82506e86bf0
meyerhenning <henning@sealedcore.tech>
2023-05-14 15:27:52 +0200

    feat(Csharp.Minimal): set up in-memory database

--- Csharp.Minimal.Api/Program.cs
+++ Csharp.Minimal.Api/Program.cs
@@ -1,4 +1,12 @@
+using Csharp.Minimal.Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApiContext>(
+    o =>
+        o.UseInMemoryDatabase("ApiDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
