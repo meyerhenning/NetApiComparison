53faad7a0fbb6f9f89490f8f214a45462183309d
meyerhenning <henning@sealedcore.tech>
2023-05-14 15:07:53 +0200

    feat(Csharp.Controller): add ApiContext

---
+++ Csharp.Controller.Api/ApiContext.cs
@@ -0,0 +1,23 @@
+using Csharp.Controller.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Csharp.Controller.Api;
+
+/// <summary>
+/// The db context representing all data used in the api.
+/// </summary>
+public class ApiContext
+    : DbContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApiContext"/> class.
+    /// </summary>
+    /// <param name="options">The db context options.</param>
+    public ApiContext(DbContextOptions options)
+        : base(options) { }
+    
+    /// <summary>
+    /// Gets or sets the <see cref="DbSet{TEntity}"/> managing students.
+    /// </summary>
+    public DbSet<Student> Students { get; set; }
+}
