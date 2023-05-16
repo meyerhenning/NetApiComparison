5b49826bfa375eed2ab8b457c49273249c1ebb53
meyerhenning <henning@sealedcore.tech>
2023-05-16 18:40:34 +0200

    feat(Fsharp.Minimal.Api): set up in-memory database

--- Fsharp.Minimal.Api/Program.fs
+++ Fsharp.Minimal.Api/Program.fs
@@ -1,15 +1,25 @@
+open Fsharp.Minimal.Api
 open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.Hosting
+
+let exitCode = 0
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    
+    builder.Services.AddDbContext<ApiContext>(
+        fun o -> o.UseInMemoryDatabase("ApiDb")
+                 |> ignore)
+    |> ignore
+    
     let app = builder.Build()
 
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
 
     app.Run()
 
-    0 // Exit code
-
+    exitCode
