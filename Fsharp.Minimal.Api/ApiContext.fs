dc0ff2b8a364f220d2d0846ab12252d4de1cc15c
meyerhenning <henning@sealedcore.tech>
2023-05-16 18:32:33 +0200

    feat(Fsharp.Minimal.Api): add db context

---
+++ Fsharp.Minimal.Api/ApiContext.fs
@@ -0,0 +1,20 @@
+namespace Fsharp.Minimal.Api
+
+open Fsharp.Minimal.Api.Models
+open Microsoft.EntityFrameworkCore
+
+/// <summary>
+/// The db context representing all data used in the api.
+/// </summary>
+type ApiContext(options: DbContextOptions<ApiContext>) =
+    inherit DbContext(options)
+    
+    [<DefaultValue>]
+    val mutable private _students: DbSet<Student>
+    
+    /// <summary>
+    /// Gets or sets the <see cref="DbSet{EntityT}"/> managing students.
+    /// </summary>
+    member x.Students
+        with get() = x._students
+        and set value = x._students <- value
