bfc4ca16f090156bace22960d771c38bcaa7549f
meyerhenning <henning@sealedcore.tech>
2023-05-16 18:55:18 +0200

    feat(Fsharp.Minimal.Api): add swagger

--- Fsharp.Minimal.Api/Program.fs
+++ Fsharp.Minimal.Api/Program.fs
@@ -1,25 +1,33 @@
+#nowarn "20"
+
 open Fsharp.Minimal.Api
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.Hosting
-
 let exitCode = 0
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
-    
+
     builder.Services.AddDbContext<ApiContext>(
         fun o -> o.UseInMemoryDatabase("ApiDb")
-                 |> ignore)
-    |> ignore
-    
+                |> ignore)
+
+    builder.Services.AddEndpointsApiExplorer()
+    builder.Services.AddSwaggerGen()
+
     let app = builder.Build()
 
+    if app.Environment.IsDevelopment() then
+        app.UseSwagger()
+        app.UseSwaggerUI()
+        |> ignore
+    
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
-
+    
     app.Run()
 
     exitCode
