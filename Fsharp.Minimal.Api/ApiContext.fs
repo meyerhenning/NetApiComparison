33a4d0264010cccf1b689a204948d282e6ffe279
meyerhenning <henning@sealedcore.tech>
2023-05-22 16:23:22 +0200

    feat(Fsharp.Minimal.Api): add teacher db set to db context

--- Fsharp.Minimal.Api/ApiContext.fs
+++ Fsharp.Minimal.Api/ApiContext.fs
@@ -12,9 +12,19 @@ type ApiContext(options: DbContextOptions<ApiContext>) =
     [<DefaultValue>]
     val mutable private _students: DbSet<Student>
     
+    [<DefaultValue>]
+    val mutable private _teachers: DbSet<Teacher>
+    
     /// <summary>
     /// Gets or sets the <see cref="DbSet{EntityT}"/> managing students.
     /// </summary>
     member x.Students
         with get() = x._students
         and set value = x._students <- value
+
+    /// <summary>
+    /// Gets or sets the <see cref="DbSet{EntityT}"/> managing teachers.
+    /// </summary>
+    member x.Teachers
+        with get() = x._teachers
+        and set value = x._teachers <- value
