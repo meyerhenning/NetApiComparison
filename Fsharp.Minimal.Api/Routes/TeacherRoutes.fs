d3a73c8225656d6ec4e86c416cd0a8abcb8937df
meyerhenning <henning@sealedcore.tech>
2023-05-22 16:23:45 +0200

    feat(Fsharp.Minimal.Api): add TeacherRoutes

--- Fsharp.Minimal.Api/Routes/TeacherRoutes.fs
+++ Fsharp.Minimal.Api/Routes/TeacherRoutes.fs
@@ -1,45 +1,45 @@
 namespace Fsharp.Minimal.Api.Routes
 
 [<RequireQualifiedAccess>]
-module StudentRoutes =
+module TeacherRoutes =
 
     /// <summary>
-    /// The base route of all student endpoints.
+    /// The base route of all teacher endpoints.
     /// </summary>
     [<Literal>]
-    let Base = "/api/Students"
+    let Base = "/api/Teachers"
     
     /// <summary>
     /// The route to the endpoint with the action:
-    /// add student
+    /// add teacher
     /// </summary>
     [<Literal>]
     let Add = Base
     
     /// <summary>
     /// The route to the endpoint with the action:
-    /// get student
+    /// get teacher
     /// </summary>
     [<Literal>]
     let Get = Base + "/{id:guid}"
     
     /// <summary>
     /// The route to the endpoint with the action:
-    /// get all students
+    /// get all teachers
     /// </summary>
     [<Literal>]
     let GetAll = Base
     
     /// <summary>
     /// The route to the endpoint with the action:
-    /// delete student
+    /// delete teacher
     /// </summary>
     [<Literal>]
     let Delete = Get
     
     /// <summary>
     /// The route to the endpoint with the action:
-    /// update student
+    /// update teacher
     /// </summary>
     [<Literal>]
     let Update = Get
